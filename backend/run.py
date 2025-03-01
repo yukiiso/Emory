15732d2e40a99d8e0bdd3503e9fe6bca53f7570a
yukiiso <yukiiso@student.ubc.ca>
2025-02-28 23:50:08 -0800

    Finalized run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from config import FLASK_HOST, FLASK_PORT, FLASK_DEBUG
 
-app = create_app()
+app = create_app(test=False)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host=FLASK_HOST, port=FLASK_PORT, debug=FLASK_DEBUG)
