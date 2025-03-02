6c7ff3339bf593f0f86e8a8cb5c40a7c2114e782
yukiiso <yukiiso@student.ubc.ca>
2025-03-02 07:00:37 -0800

    config added

--- backend/config.py
+++ backend/config.py
@@ -61,3 +61,12 @@ class AWSClient:
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name=AWS_REGION
         )
+    
+    @staticmethod
+    def init_transcribe_client():
+        return boto3.client(
+            "transcribe",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_REGION
+        )
