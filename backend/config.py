a7b8d2559dc183f92b6cdc260677b324f9dab26c
yukiiso <yukiiso@student.ubc.ca>
2025-03-02 07:58:52 -0800

    Added comprehend config

--- backend/config.py
+++ backend/config.py
@@ -86,3 +86,12 @@ class AWSClient:
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name=AWS_REGION
         )
+    
+    @staticmethod
+    def init_comprehend_client():
+        return boto3.client(
+            "transcribe",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_REGION
+        )
